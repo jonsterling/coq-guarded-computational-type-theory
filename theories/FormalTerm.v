1ea728f66d5d22f7eb3c7cfca1de8b53ac33eea2
Jon Sterling <jon@jonmsterling.com>
2017-10-30 16:44:58 -0400

    define interpretation of 'formal terms' into 'real terms'

--- theories/FormalTerm.v
+++ theories/FormalTerm.v
@@ -1,8 +1,14 @@
 Require Import Unicode.Utf8.
+Require Import Coq.Vectors.Vector.
+From gctt Require Import Terms.
+From gctt Require Import Axioms.
 
 Set Implicit Arguments.
 
 Module FTm.
+  Inductive clk l :=
+  | cref : ∀ k, k < l → clk l.
+
   Inductive t (l n : nat) :=
   | var : forall i, i < n -> t l n
   | fst : t l n -> t l n
@@ -15,7 +21,42 @@ Module FTm.
   | prod : t l n -> t l n -> t l n
   | arr : t l n -> t l n -> t l n
   | pair : t l n -> t l n -> t l n
-  | ltr : ∀ k, k < l → t l n -> t l n
+  | ltr : clk l → t l n -> t l n
   | isect : t (S l) n -> t l n
   | univ : nat -> t l n.
 End FTm.
+
+Definition Env := Vector.t CLK.
+
+
+Program Definition interp_clk {l : nat} (k : FTm.clk l) (ρ : Env l) : CLK :=
+  match k with
+  | FTm.cref _ p => Vector.nth_order ρ p
+  end.
+
+Reserved Notation "⟦ e ⟧ ρ" (at level 50).
+Notation "K⟦ k ⟧ ρ" := (interp_clk k ρ) (at level 50).
+
+Definition extend {l : nat} (κ : CLK) (ρ : Env l) : Env (S l) :=
+  Vector.cons _ κ _ ρ.
+
+Notation "κ ∷ ρ" := (Vector.cons _ κ _ ρ) (at level 30).
+
+Program Fixpoint interp {l n : nat} (e : FTm.t l n) (ρ : Env l) : Tm.t n :=
+  match e with
+  | FTm.var i p => Tm.var p
+  | FTm.fst e => Tm.fst (⟦e⟧ ρ)
+  | FTm.snd e => Tm.snd (⟦e⟧ ρ)
+  | FTm.unit => Tm.unit
+  | FTm.bool => Tm.bool
+  | FTm.ax => Tm.ax
+  | FTm.tt => Tm.tt
+  | FTm.ff => Tm.ff
+  | FTm.prod A B => Tm.prod (⟦A⟧ ρ) (⟦B⟧ ρ)
+  | FTm.arr A B => Tm.arr (⟦A⟧ ρ) (⟦B⟧ ρ)
+  | FTm.pair A B => Tm.pair (⟦A⟧ ρ) (⟦B⟧ ρ)
+  | FTm.ltr ref A => Tm.ltr (K⟦ref⟧ ρ) (⟦A⟧ ρ)
+  | FTm.isect A => Tm.isect (fun κ => ⟦A⟧ (κ ∷ ρ))
+  | FTm.univ i => Tm.univ i
+  end
+where "⟦ e ⟧ ρ" := (interp e ρ).
