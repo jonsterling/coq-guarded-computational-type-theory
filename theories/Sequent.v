c8b49ec4749a94a7a1ccf70e52e9dbe9e8b148ec
Jon Sterling <jon@jonmsterling.com>
2017-11-27 13:00:20 -0500

    define versions of judgments with presuppositions

--- theories/Sequent.v
+++ theories/Sequent.v
@@ -16,18 +16,18 @@ Inductive Prectx : Var.Ctx → Type :=
 Notation "⋄" := nil.
 Infix ";" := (snoc) (at level 50, left associativity).
 
+
 Definition atomic_eq_ty (τ : matrix) (A B : Tm.t 0) :=
   ∃ R, τ (A, R) ∧ τ (B, R).
 
 Definition atomic_eq_mem (τ : matrix) (A e1 e2 : Tm.t 0) :=
   ∃ R, τ (A, R) ∧ R (e1, e2).
 
-
 Notation "τ ⊧ A ∼ B" := (atomic_eq_ty τ A B) (at level 10).
 Notation "τ ⊧ A ∋ e1 ∼ e2" := (atomic_eq_mem τ A e1 e2) (at level 10).
 Reserved Notation "τ ⊧ Γ ∋⋆ γ1 ∼ γ2" (at level 10).
 
-Program Fixpoint atomic_eq_env {Ψ} (τ : matrix) (Γ : Prectx Ψ) (γ1 γ2 : Tm.Sub.t Ψ 0) : Prop :=
+Program Fixpoint atomic_eq_env {Ψ} τ Γ (γ1 γ2 : Tm.Sub.t Ψ 0) : Prop :=
   match Γ with
   | ⋄ => True
   | Γ ; A =>
@@ -47,16 +47,25 @@ Program Fixpoint is_ctx {Ψ} (τ : matrix) (Γ : Prectx Ψ) : Prop :=
   end
 
 with
-seq_eq_ty {Ψ} (τ : matrix) (Γ : Prectx Ψ) (A B : Tm.t Ψ) : Prop :=
-  ∀ γ1 γ2 : Tm.Sub.t Ψ 0,
+seq_eq_ty {Ψ} τ Γ (A B : Tm.t Ψ) : Prop :=
+  ∀ γ1 γ2,
     τ ⊧ Γ ∋⋆ γ1 ∼ γ2
     → τ ⊧ (Tm.subst γ1 A) ∼ (Tm.subst γ2 B)
 where "τ ⊧ Γ 'ctx'" := (is_ctx τ Γ)
 and "τ ⊧ Γ ≫ A ∼ B" := (seq_eq_ty τ Γ A B).
 
-Definition seq_eq_mem {Ψ} (τ : matrix) (Γ : Prectx Ψ) (A e1 e2 : Tm.t Ψ) :=
-  ∀ γ1 γ2 : Tm.Sub.t Ψ 0,
+Definition seq_eq_mem {Ψ} τ Γ (A e1 e2 : Tm.t Ψ) :=
+  ∀ γ1 γ2,
     τ ⊧ Γ ∋⋆ γ1 ∼ γ2
     → τ ⊧ (Tm.subst γ1 A) ∋ (Tm.subst γ1 e1) ∼ (Tm.subst γ2 e2).
 
 Notation "τ ⊧ Γ ≫ A ∋ e1 ∼ e2" := (seq_eq_mem τ Γ A e1 e2) (at level 10).
+
+Definition full_seq_eq_ty {Ψ} τ Γ (A B : Tm.t Ψ) `{τ ⊧ Γ ctx} :=
+  τ ⊧ Γ ≫ A ∼ B.
+
+Definition full_seq_eq_mem {Ψ} τ Γ (A e1 e2 : Tm.t Ψ) `{H : τ ⊧ Γ ctx} `{τ ⊧ Γ ≫ A ∼ A} :=
+  τ ⊧ Γ ≫ A ∋ e1 ∼ e2.
+
+Notation "τ ⊧ Γ ≫ A ≐ B" := (full_seq_eq_ty τ Γ A B) (at level 10).
+Notation "τ ⊧ Γ ≫ A ∋ e1 ≐ e2" := (full_seq_eq_mem τ Γ A e1 e2) (at level 10).
